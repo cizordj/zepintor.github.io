39f8955861dc1aa52e8f42c1e8e69c69997afd2a
cizordj <32869222+cizordj@users.noreply.github.com>
2020-06-02 17:23:37 -0300

    Update contact formulary
    - Now the formulary uses the native m4q library to submit forms.

--- js/custom.js
+++ js/custom.js
@@ -87,22 +87,21 @@ function randomPage(){
     var hash = pages[index];
     window.location.hash = hash;
 }
-function submitRequest() {
-    var method = document.getElementById("formulary").method;
-    var data = new FormData(document.getElementById("formulary"));
-    var url = document.getElementById("formulary").action;
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState !== XMLHttpRequest.DONE)
-            return;
-        if (xhr.status === 200) {
+function submitRequest(){
+    var form = document.getElementById('formulary');
+    var headers = { Accept: 'application/json' };
+    $.ajax({url: form.action,
+        method: form.method,
+        data: form,
+        headers: headers
+    }).then(
+        function(response){
             Metro.infobox.create("<h1>Pedido enviado com sucesso!</h1><p>A sua solicitação foi efetuada com sucesso, assim que pudermos retornaremos o mais breve possível.</p>", "success");
             window.location.hash="home";
-        } else {
+            form.reset();
+        },
+        function(xhr){
             Metro.infobox.create("<h1>Opss...</h1><p>Alguma coisa deu errada, verifique se você não enviou um formulário vazio ;)</p>", "bg-darkCrimson fg-white");
         }
-    };
-    xhr.send(data);
+    );
 }
